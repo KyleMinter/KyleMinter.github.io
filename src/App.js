5d75351ef5d8ae6d1672925c03590237597eeb19
Kyle Minter <kyle.minter.1@gmail.com>
2024-08-31 17:42:35 -0500

    Consolidated BioPage.js and removed unused file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import './App.css'
-import BioPage from './bio_page/BioPage';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import BioPageContent from './bio_page/BioPageContent';
 import PortfolioTabs from './portfolio_page/PortfolioTabs';
 import PageFooter from './footers/PageFooter';
 
 function App() {
   return (
     <div className="App bg-primary">
+
       <div id="bio_page">
-        <BioPage/>
+        <Container fluid className="min-vh-100 d-flex flex-column px-0 bg-dark text-light">
+            <Row className="flex-grow-1">
+                <Col className="align-self-center">
+                    <BioPageContent/>
+                </Col>
+            </Row>
+
+            <PageFooter icon={"bi bi-chevron-bar-down"} text={"View Portfolio Below"} link={"#portfolio_tabs"} />
+        </Container>
       </div>
+
       <div id="portfolio_tabs" className="p-5">
         <PortfolioTabs/>
+        <PageFooter icon={"bi bi-chevron-bar-up"} text={"Back to the top"} link={"#bio_page"} />
       </div>
-      <PageFooter icon={"bi bi-chevron-bar-up"} text={"Back the top"} link={"#bio_page"} />
     </div>
   );
 }
