8c1ce9e33a63d1b10ae389f99ddb32aed5dd7277
Kyle Minter <kyle.minter.1@gmail.com>
2024-09-02 05:09:39 -0500

    Removed some useless styling

--- src/icons/IconBadge.js
+++ src/icons/IconBadge.js
@@ -9,7 +9,7 @@ export default function IconBadge({icon = "bi bi-question-circle-fill", text = "
             <Badge className="bg-primary hover-gradient m-1 p-0">
                     <Stack direction="horizontal">
                         <div className="bg-gradient rounded-start-2 p-2">
-                            <i className={icon} styles="font-size: 2rem;"/>
+                            <i className={icon}/>
                         </div>
                         <div className="vr"/>
                         <div className="p-2">{text}</div>
