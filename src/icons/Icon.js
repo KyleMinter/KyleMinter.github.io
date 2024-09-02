8de588db822965d6aab6459f833939bf8e674337
Kyle Minter <kyle.minter.1@gmail.com>
2024-09-02 05:25:04 -0500

    Slight style changes

--- src/icons/Icon.js
+++ src/icons/Icon.js
@@ -5,9 +5,9 @@ export default function Icon({icon = "bi bi-question-circle-fill", link = null})
 {
     return (
         <a href={link}>
-            <Badge className="hover-gradient text-light p-2">
-                <i className={icon} styles="font-size: 2rem;"/>
-            </Badge>
+            <div className="hover-gradient bg-primary rounded-2 text-light px-1">
+                <i className={icon} />
+            </div>
         </a>
     );
 }
