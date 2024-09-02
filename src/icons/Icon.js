edf840a3e71a7b26f2e43362344844bb0b5cc438
Kyle Minter <kyle.minter.1@gmail.com>
2024-09-02 05:36:38 -0500

    Removed background on Icon and changed hover effect to darken

--- src/icons/Icon.js
+++ src/icons/Icon.js
@@ -4,10 +4,8 @@ import Badge from 'react-bootstrap/Badge';
 export default function Icon({icon = "bi bi-question-circle-fill", link = null})
 {
     return (
-        <a href={link}>
-            <div className="hover-gradient bg-primary rounded-2 text-light px-1">
-                <i className={icon} />
-            </div>
+        <a href={link} className="hover-darken text-light">
+            <i className={icon} />
         </a>
     );
 }
