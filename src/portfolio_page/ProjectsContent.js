ebd97545cc736fd83315350013a76e6d67a383af
Kyle Minter <kyle.minter.1@gmail.com>
2024-09-02 15:03:32 -0500

    Made it more clear that the project card can be extended

--- src/portfolio_page/ProjectsContent.js
+++ src/portfolio_page/ProjectsContent.js
@@ -11,21 +11,22 @@ const renderCards = (content) => {
     const renderedIconBadges = content.iconBadges.map((iconBadges) => renderIconBadges(iconBadges));
     
     return React.createElement(() => (
-        <Container className="project-card m-2 p-0 rounded-2 d-flex flex-column overflow-hidden" style={{height: "16rem", maxWidth: "25rem",}}>
+        <Container className="project-card m-2 p-0 rounded-2 d-flex flex-column overflow-hidden text-start" style={{height: "16rem", maxWidth: "25rem",}}>
             <Container className="bg-primary p-0 flex-grow-0 flex-shrink-0 d-inline-block overflow-hidden position-relative h-75">
                 <img src={require(`${content.thumbnail}`)} alt="" className="object-fit-cover w-100 h-100"/>
             </Container>
             
-            <Container className="sliding-tab bg-dark p-2 text-start d-flex align-items-center flex-grow-0 flex-shrink-0 h-25">
-                    <h5 className="p-2 flex-grow-1">{content.title}</h5>
-                    {renderedIcons}
+            <Container className="sliding-tab bg-dark px-2 d-flex align-items-center flex-grow-0 flex-shrink-0 h-25">
+                <h5 className="px-2 flex-grow-1">{content.title}</h5>
+                <i className="fs-2 pb-1 px-3 bi bi-chevron-bar-up"/>
             </Container>
 
-            <Container className="bg-dark sliding-tab text-start border-top border-bottom border-tertiary p-1 flex-grow-0 flex-shrink-0">
-                {renderedIconBadges}
+            <Container className="bg-dark sliding-tab border-top border-bottom border-tertiary p-1 flex-grow-0 flex-shrink-0 d-flex">
+                <div className="flex-grow-1">{renderedIconBadges}</div>
+                <div className="d-flex flex-nowrap">{renderedIcons}</div>
             </Container>
 
-            <Container className="bg-primary sliding-tab text-start px-3 py-2 flex-grow-0 flex-shrink-0 h-100">
+            <Container className="bg-primary sliding-tab px-3 py-2 flex-grow-0 flex-shrink-0 h-100">
                     <p>{content.description}</p>
             </Container>
         </Container>
